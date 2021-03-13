78ce53c16fc28276a7545f7e7f051c3d858d7010
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-03-13 11:20:11 +0000

    Add Google map

--- blocks/map.php
+++ blocks/map.php
@@ -7,5 +7,5 @@
 ?>
 
 <div class="map">
-	DISPLAY THE MAP
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.4893801653425!2d-73.975324484095!3d40.75125997932772!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25900ffffffff%3A0x8745812ccb51ab0d!2sJ%20A%20Jennings%20Inc!5e0!3m2!1sen!2suk!4v1615634245869!5m2!1sen!2suk" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 </div>
