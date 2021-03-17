ce4a06ca1703fd69220f3a68c22b92c008d383d2
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-03-17 12:29:03 +0000

    Update gallery

--- blocks/gallery.php
+++ blocks/gallery.php
@@ -8,12 +8,41 @@
 $content = get_field('gallery-content');
 ?>
 
+<?php
+$filter_terms = array();
+$gallery_items = get_field('gallery-items');
+
+// return an array of taxonomy names
+if( $gallery_items ) {
+	foreach( $gallery_items as $gallery_item ) {
+		
+		$type = $gallery_item['is_image'];
+		
+		if ( $type == 'image' ) {
+			$term_ids = $gallery_item['gallery-image-market'];
+			foreach ( $term_ids as $term_id ) {
+				$term = get_term( $term_id, 'market' );
+				$filter_terms[] = $term->name;
+			}
+		} else {
+			$terms = get_the_terms( $gallery_item['project'], 'market' );
+			foreach ($terms as $term) {
+				$filter_terms[] = $term->name;
+			}
+		}	
+		
+	}
+}
+?>
+
 <div class="container">
+	
 	<?php if ( $content ) { ?>
 	<div class="gallery__content">
 		<?php echo $content; ?>
 	</div>
 	<?php } ?>
+	
 	<div class="gallery__buttons">
 		<p>Filter:</p>
 		<button data-filter="*">All</button>
@@ -21,76 +50,79 @@ $content = get_field('gallery-content');
 		<button data-filter=".project">Case Study</button>
 
 		<p>Markets:</p>
-		<button data-filter="*">All</button>
-		<button data-filter=".health">Health</button>
-		<button data-filter=".finance">Finance</button>
+		<select class="filters-select">
+			<option value="*">All</option>
+			<?php
+			$filter_terms = array_unique($filter_terms); 
+			foreach ( $filter_terms as $filter_term ) {
+				echo '<option value=".' . sanitize_title($filter_term) . '">' . $filter_term . '</option>';
+			}
+			?>
+		</select>
 	</div>
+	
 	<div class="gallery">
-	<?php if( have_rows('gallery-items') ) { ?>
-		<?php while( have_rows('gallery-items') ) { the_row();
-			$type = get_sub_field('is_image');
-			$image = get_sub_field('image');
-			$projects = get_sub_field('project');
-		?>
-		<?php if( $type == 'image' ) { ?>
-			<?php if( have_rows('image') ) { ?>
-				<?php while( have_rows('image') ) { the_row();
-					$image = get_sub_field('gallery-image');
-					$title = get_sub_field('gallery-image-title');
-					$size = get_sub_field('gallery-image-size');
-					$market = get_sub_field('gallery-image-market', 'market');
-				?>
-				<div class="gallery-item <?php echo $size ?> <?php echo $marlet ?> image">
-					<a href="<?php echo $image['url']; ?>" class="gallery-item__link modal"><img src="<?php echo $image['url']; ?>" alt="">
-						<?php if ( $title ) { ?>
-						<div class="gallery-item__link-title">
-							<p><?php echo $title ?></p>
-						</div>
-						<?php } ?>
-					</a>
+		
+	<?php
+	if( $gallery_items ) { 
+		foreach( $gallery_items as $gallery_item ) {
+		$type = $gallery_item['is_image'];
+			if ( $type == 'image' ) {
+	?>
+	
+	<?php 		
+		$image = $gallery_item['gallery-image'];
+		$title = $gallery_item['gallery-image-title'];
+		$size = $gallery_item['gallery-image-size'];
+		$term_ids = $gallery_item['gallery-image-market'];
+		$markets = array();
+		foreach ( $term_ids as $term_id ) {
+			$term = get_term( $term_id, 'market' );
+			$markets[] = sanitize_title($term->name);
+		}
+	?>
+
+		<div class="gallery-item test-item <?php echo $size; ?> <?php foreach ( $markets as $market ) { echo $market . ' '; } ?> image">
+			<a href="<?php echo $image['url']; ?>" class="gallery-item__link modal">
+				<img src="<?php echo $image['url']; ?>" alt="">
+				<?php if ( $title ) { ?>
+				<div class="gallery-item__link-title">
+					<p><?php echo $title ?></p>
 				</div>
 				<?php } ?>
-			<?php } ?>
-			<?php } else { ?>
-			<?php if( $projects ) { ?>
-				<?php foreach( $projects as $project):
-					setup_postdata( $project );
-				?>
-				<?php
-					$title = get_the_title($project);
-					$image = get_field( 'project-image', $project);
-					$terms = get_the_terms(($project), 'market' );
-				?>
-				<div class="gallery-item gallery-item--tall project">
-					<a href="<?php the_permalink($project); ?>" class="gallery-item__project">
-						<img src="<?php echo $image['project-image-featured']['url']; ?>" alt="<?php echo $image['project-image-featured']['alt']; ?>">
-						<div class="gallery-item__project-title">
-							<h3><?php echo $title ?></h3>
-							<?php
-								foreach ( $terms as $term ) {
-								echo '<p>' .$term->name . '</p>';
-							} ?>
-							<svg class="icon icon--arrow"><use xlink:href="#arrow"></use></svg>
-						</div>
-					</a>
+			</a>
+		</div>
+
+	<?php } else { ?>
+	
+	<?php		
+		$markets = array();
+		$project_id = $gallery_item['project'];
+		$terms = get_the_terms( $project_id, 'market' );
+		$image = get_field('project-image', $project_id);
+		$featured_image = $image['project-image-featured']['url'];
+		
+		foreach ($terms as $term) {
+			$markets[] = $term->name;
+		}
+	?>
+		<div class="gallery-item gallery-item--tall <?php foreach ( $markets as $market ) { echo sanitize_title($market) . ' '; } ?> project">
+			<a href="<?php the_permalink($project_id); ?>" class="gallery-item__project">
+				<img src="<?php echo $featured_image; ?>" alt="">
+				<div class="gallery-item__project-title">
+					<h3><?php echo get_the_title($project_id); ?></h3> 
+					<?php foreach ( $markets as $market ) { echo '<p>' . $market . '</p>'; } ?>
+					<svg class="icon icon--arrow"><use xlink:href="#arrow"></use></svg>
 				</div>
-
-				<?php endforeach; ?>
-				<?php wp_reset_postdata(); ?>
-			<?php } ?>
-		<?php } ?>
-		<?php }?>
-	<?php } ?>
+			</a>
+		</div>
+		
+	<?php 
+			}
+		}
+	}
+	?>
+		
 	</div>
-
-
-	<div class="gallery">
-		<div class="gallery-item gallery-item--tall health image"><a href="/wp-content/uploads/2021/03/project-banner.png" class="gallery-item__link modal"><img src="/wp-content/uploads/2021/03/project-banner.png" alt=""><div class="gallery-item__link-title"><p>Northwell Cohen’s Children’s Hospital</p></div></a></div>
-		<div class="gallery-item health image"><a href="/wp-content/uploads/2021/03/service-banner-image.png" class="gallery-item__link modal"><img src="/wp-content/uploads/2021/03/project-banner.png" alt=""><div class="gallery-item__link-title"><p>Columbia Data Center</p></div></a></div>
-		<div class="gallery-item gallery-item--tall health project"><a href="" class="gallery-item__project"><img src="/wp-content/uploads/2021/03/project-banner.png" alt=""><div class="gallery-item__project-title"><h3>Project title</h3> <p>Market</p> <svg class="icon icon--arrow"><use xlink:href="#arrow"></use></svg></div></a></div>
-		<div class="gallery-item finance image"><a href="/wp-content/uploads/2021/03/service-banner-image.png" class="gallery-item__link modal"><img src="/wp-content/uploads/2021/03/project-banner.png" alt=""><div class="gallery-item__link-title"><p>Columbia Data Center</p></div></a></div>
-		<div class="gallery-item finance image"><a href="/wp-content/uploads/2021/03/service-banner-image.png" class="gallery-item__link modal"><img src="/wp-content/uploads/2021/03/project-banner.png" alt=""><div class="gallery-item__link-title"><p>Columbia Data Center</p></div></a></div>
-		<div class="gallery-item finance image"><a href="/wp-content/uploads/2021/03/service-banner-image.png" class="gallery-item__link modal"><img src="/wp-content/uploads/2021/03/project-banner.png" alt=""><div class="gallery-item__link-title"><p>Columbia Data Center</p></div></a></div>
-		<div class="gallery-item image"><a href="/wp-content/uploads/2021/03/service-banner-image.png" class="gallery-item__link modal"><img src="/wp-content/uploads/2021/03/service-banner-image.png" alt=""><div class="gallery-item__link-title"><p>Columbia Data Center</p></div></a></div>
-	</div>
+	
 </div>
--- src/js/main.js
+++ src/js/main.js
@@ -85,6 +85,31 @@ jQuery(document).ready(function($) {
 		var filterValue = $(this).attr('data-filter');
 		$grid.isotope({ filter: filterValue });
 	});
+	// filter functions
+	var filterFns = {
+		// show if number is greater than 50
+		numberGreaterThan50: function() {
+		  var number = $(this).find('.number').text();
+		  return parseInt( number, 10 ) > 50;
+		},
+		// show if name ends with -ium
+		ium: function() {
+		  var name = $(this).find('.name').text();
+		  return name.match( /ium$/ );
+		}
+	};
+	// bind filter on select change
+	$('.filters-select').on( 'change', function() {
+	// get filter value from option value
+	var filterValue = this.value;
+	// use filterFn if matches value
+	filterValue = filterFns[ filterValue ] || filterValue;
+	$grid.isotope({ filter: filterValue });
+	});
+	
+  
+
+
 
 	// Lightbox
 	$('.modal').featherlightGallery({
@@ -93,6 +118,7 @@ jQuery(document).ready(function($) {
 		galleryFadeIn: 300,
 		galleryFadeOut: 300
 	});
+	
 
 
 	// The function actually applying the offset
