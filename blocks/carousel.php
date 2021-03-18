1c09333998612d327ca13e4ee292de5dac72d1e1
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-03-18 14:29:40 +0000

    Add market to carousel

--- blocks/carousel.php
+++ blocks/carousel.php
@@ -26,7 +26,12 @@ $projects = get_field( 'carousel' );
 			$terms = get_the_terms(($project), 'market' );
 		?>
 		<div class="carousel-slider-item">
-			<div class="carousel-slider-item__title"><p><?php echo $title ?></p></div>
+			<div class="carousel-slider-item__title"><p><?php echo $title ?>
+			<br />
+			<?php
+				foreach ( $terms as $term ) {
+				echo $term->name;
+			} ?></p></div>
 			<img src="<?php echo $image['project-image-featured']['url']; ?>" alt="<?php echo $image['project-image-featured']['alt']; ?>" />
 		</div>
 		<?php endforeach; ?>
